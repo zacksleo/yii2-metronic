06e3b8ced0f8da6d235bc92038e7d2aec24b870a
zacksleo <zacksleo@gmail.com>
2016-12-13 14:20:48 +0800

    add jquery-repeater & multi-select

---
+++ bundles/JqueryRepeaterAsset.php
@@ -0,0 +1,25 @@
+<?php
+namespace zacksleo\yii2\metronic\bundles;
+
+use yii\web\AssetBundle;
+use yii\web\View;
+
+class JqueryRepeaterAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/jquery.repeater';
+    public $js = [
+        'jquery.repeater.min.js'
+    ];
+    public $publishOptions = [
+        'except'=>[
+            'src',
+            'test'
+        ]
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+    public $jsOptions = [
+        'position' => View::POS_END
+    ];
+}
---
+++ bundles/MultiSelectAsset.php
@@ -0,0 +1,30 @@
+<?php
+namespace zacksleo\yii2\metronic\bundles;
+
+use yii\web\AssetBundle;
+use yii\web\View;
+
+class MultiSelectAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/multiselect';
+    public $publishOptions = [
+        'only' => [
+            'css/*',
+            'img/*',
+            'js/*'
+        ]
+    ];
+    public $css = [
+        'css/multi-select.css'
+    ];
+    public $js = [
+        'js/jquery.multi-select.js'
+    ];
+    public $jsOptions = [
+        'position' => View::POS_END
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+}
