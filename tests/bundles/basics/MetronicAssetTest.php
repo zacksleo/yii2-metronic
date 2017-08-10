657439e0604ff04525bb2d8a1759d04412f8c2fb
zacksleo <zacksleo@gmail.com>
2017-08-10 14:37:51 +0800

    add unit test

---
+++ tests/bundles/basics/MetronicAssetTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace tests\bundles\basics;
+
+use zacksleo\yii2\metronic\bundles\basics\MetronicAsset;
+use Yii;
+use yii\web\AssetBundle;
+use tests\TestCase;
+
+/**
+ * MetronicAssetTest
+ */
+class MetronicAssetTest extends TestCase
+{
+    public function testAsset()
+    {
+        $view = $this->getView();
+        $this->assertEmpty($view->assetBundles);
+        MetronicAsset::register($view);
+        $this->assertEquals(5, count($view->assetBundles));
+        $this->assertArrayHasKey('zacksleo\\yii2\\metronic\\bundles\\basics\\MetronicAsset', $view->assetBundles);
+        $this->assertTrue($view->assetBundles['zacksleo\\yii2\\metronic\\bundles\\basics\\MetronicAsset'] instanceof AssetBundle);
+        $content = $view->renderFile('@tests/data/views/layout.php');
+        $this->assertContains('global/css/components-md.min.css', $content);
+        $this->assertContains('global/css/plugins-md.min.css', $content);
+        $this->assertContains('global/scripts/app.min.js', $content);
+        $this->assertContains('layouts/global/scripts/quick-sidebar.min.js', $content);
+
+    }
+}
